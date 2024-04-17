5d74051ad7f7c3bb21600c74c6bd212113cdc725
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-17 06:04:32 +0200

    Minor fix - adding closing php tag

--- www/hello.php
+++ www/hello.php
@@ -1,15 +1,17 @@
 <?php
 
-$message =  "Hello again!";
+  $message =  "Hello again!";
 
-$count = 3;
-$price = 1.99;
+  $count = 3;
+  $price = 1.99;
 
-$is_admin = true;
-$data = null;
+  $is_admin = true;
+  $data = null;
 
-var_dump($message);
-var_dump($count);
-var_dump($price);
+  var_dump($message);
+  var_dump($count);
+  var_dump($price);
 
-var_dump($is_admin, $data);
+  var_dump($is_admin, $data);
+
+?>
