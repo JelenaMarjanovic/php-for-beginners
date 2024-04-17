5c7219f83eb9accf836f474507d312fe5c5529a9
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-18 01:51:30 +0200

    Strings: Using single or double quotes and variable interpolation

--- www/hello.php
+++ www/hello.php
@@ -1,17 +1,51 @@
 <?php
 
-  $message =  "Hello again!";
+  $message = "Hello";
+  $name = "Dave";
 
-  $count = 3;
-  $price = 1.99;
+  $count = 10;
+  $size = 2;
 
-  $is_admin = true;
-  $data = null;
+  $price = 2.95;
+  $quantity = 5;
+  $price_string = "75";
 
-  var_dump($message);
-  var_dump($count);
-  var_dump($price);
+  $is_editor = true;
+  $is_admin = false;
 
-  var_dump($is_admin, $data);
+  $greeting = 'Hello';
+  $person = "Dave";
+
+  $start = "3 o'clock";
+  $end = '4 o\'clock';
+
+  $days = "\nMonday\nTuesday\nWednesday\n";
+
+  // $is_admin = true;
+  // $data = null;
+
+  // var_dump($message);
+  // var_dump($count);
+  // var_dump($price);
+
+  // var_dump($is_admin, $data);
+
+  var_dump($count + 5);
+  var_dump($count * $size);
+
+  var_dump($price * $quantity);
+  var_dump($price_string * $quantity);
+
+  echo $message . " " . $name;
+
+  var_dump(! $is_editor);
+  var_dump($is_editor or $is_admin);
+  var_dump($is_admin and $is_editor);
+
+  echo $start, " ", $end;
+  echo $days;
+  echo "Hello, " . $person;
+  echo "Hello, $person";
+  echo "Hello {$person}";
 
 ?>
