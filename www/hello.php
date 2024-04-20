b011eb07e0e212b028860aa2fa2c854b7c0ab781
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-20 16:39:32 +0200

    Assign manual array indexes and create associative arrays

--- www/hello.php
+++ www/hello.php
@@ -1,9 +1,14 @@
 <?php
 
-  $articles = ["First post", "Another post", "Read this!"];
+  // $articles = ["First post", "Another post", "Read this!"];
+  // $articles = [1 => "First post", 3 => "Another post", "Read this!"];
+  $articles = ["first" => "First post", "second" => "Another post", "third" => "Read this!"];
 
   var_dump($articles);
-  var_dump($articles[0]);
-  var_dump($articles[2]);
+
+  // var_dump($articles[0]);
+  // var_dump($articles[2]);
+
+  var_dump($articles["second"]);
 
 ?>
