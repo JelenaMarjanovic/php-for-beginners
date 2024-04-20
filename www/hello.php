93644d942dd18a69a364c14e5069d7f9a27c8f8d
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-20 16:41:51 +0200

    Minor fix - put instruction on several lines due to readability

--- www/hello.php
+++ www/hello.php
@@ -2,7 +2,11 @@
 
   // $articles = ["First post", "Another post", "Read this!"];
   // $articles = [1 => "First post", 3 => "Another post", "Read this!"];
-  $articles = ["first" => "First post", "second" => "Another post", "third" => "Read this!"];
+  $articles = [
+    "first" => "First post",
+    "second" => "Another post",
+    "third" => "Read this!"
+  ];
 
   var_dump($articles);
 
