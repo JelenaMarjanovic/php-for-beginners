ca0fd43c18b99726d859376e3d4152246b28ac9e
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-20 05:12:04 +0200

    Introduction to Arrays: Create an array and access its elements

--- www/hello.php
+++ www/hello.php
@@ -1,51 +1,9 @@
 <?php
 
-  $message = "Hello";
-  $name = "Dave";
+  $articles = ["First post", "Another post", "Read this!"];
 
-  $count = 10;
-  $size = 2;
-
-  $price = 2.95;
-  $quantity = 5;
-  $price_string = "75";
-
-  $is_editor = true;
-  $is_admin = false;
-
-  $greeting = 'Hello';
-  $person = "Dave";
-
-  $start = "3 o'clock";
-  $end = '4 o\'clock';
-
-  $days = "\nMonday\nTuesday\nWednesday\n";
-
-  // $is_admin = true;
-  // $data = null;
-
-  // var_dump($message);
-  // var_dump($count);
-  // var_dump($price);
-
-  // var_dump($is_admin, $data);
-
-  var_dump($count + 5);
-  var_dump($count * $size);
-
-  var_dump($price * $quantity);
-  var_dump($price_string * $quantity);
-
-  echo $message . " " . $name;
-
-  var_dump(! $is_editor);
-  var_dump($is_editor or $is_admin);
-  var_dump($is_admin and $is_editor);
-
-  echo $start, " ", $end;
-  echo $days;
-  echo "Hello, " . $person;
-  echo "Hello, $person";
-  echo "Hello {$person}";
+  var_dump($articles);
+  var_dump($articles[0]);
+  var_dump($articles[2]);
 
 ?>
