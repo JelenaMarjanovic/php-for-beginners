24246b6c64b5967b58eb5a0dfc42d3a6764437d4
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-04-23 06:40:23 +0200

    Compare One Value to Another: comparison operators

--- www/hello.php
+++ www/hello.php
@@ -8,15 +8,31 @@
   //   echo "The condition is true.";
   // }
 
-  $articles = [];
+  // $articles = [];
   // $articles = ["JavaScript", "Java", "PHP"];
 
   // var_dump(empty($articles));
 
-  if (empty($articles)) {
-    echo "The array is empty.";
+  // if (empty($articles)) {
+  //   echo "The array is empty.";
+  // } else {
+  //   echo "The array is not empty.";
+  // }
+
+  // var_dump(3 == 4);
+  // var_dump(4 == 4);
+
+  $age = 21;
+  // $age = 18;
+
+  // if ($age == 21) {  // 21 - true
+  // if ($age != 21) {  // 21 - false
+  // if ($age < 21) {  // 18 - true
+  // if ($age > 21) {  // 21 - false
+  if ($age >= 21) {  // 21 - true
+    echo "Condition is true";
   } else {
-    echo "The array is not empty.";
+    echo "Condition is false";
   }
 
 ?>
